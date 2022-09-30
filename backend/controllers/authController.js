b0ee4b02fe3d4aa09c558959808399ecf14d9105
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2022-10-01 03:43:26 +0500

    Added exceptions handling to catch cloudinary errors

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,25 +10,32 @@ const cloudinary = require("cloudinary");
 
 // Register a user   => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-  // const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
-  //     folder: 'avatars',
-  //     width: 150,
-  //     crop: "scale"
-  // })
+  try {
+    const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+      folder: "avatars",
+      width: 150,
+      crop: "scale",
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 
   const { name, email, password } = req.body;
 
-  const user = await User.create({
-    name,
-    email,
-    password,
-    // avatar: {
-    //     public_id: result.public_id,
-    //     url: result.secure_url
-    // }
-  });
-
-  sendToken(user, 200, res);
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password,
+      avatar: {
+        public_id: result.public_id,
+        url: result.secure_url,
+      },
+    });
+    sendToken(user, 200, res);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 // Login User  =>  /a[i/v1/login
