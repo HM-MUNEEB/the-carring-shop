058468a36606976c8da0cc7f1dd338e29e997ae9
MUNYYB UR RYHMAN <hm.muneeb.u.r@gmail.com>
2022-11-28 18:12:34 +0500

    Added initial base user

---
+++ backend/migrations/20221128125741-seed-super-admin.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  async up(db, client) {
+    // TODO write your migration here.
+    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
+    try {
+      await db.collection("superAdmin").insertOne({
+        name: "Zain hashmi",
+        email: "zainhashmi551@gmail.com",
+        password: await bcrypt.hash("zainAdmin", 10),
+      });
+      console.log("The user 'Zain hashmi' is added.");
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+
+  async down(db, client) {
+    // TODO write the statements to rollback your migration (if possible)
+    // Example:
+    // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
+    try {
+      await db.collection("superAdmin").deleteMany();
+      console.log("All super admin users are deleted");
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+};
